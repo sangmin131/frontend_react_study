973204f0056811840d2eeb7d9d0692ad7b3a70d5
goni.kim <geoblo@naver.com>
2023-01-18 18:24:09 +0900

    수업 업로드

---
+++ 11_react/my-app/src/chapter5/5.3/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer(params) {
+  return <footer>푸터입니다.</footer>;
+}
+
+export default Footer;
--- 11_react/my-app/src/index.js
+++ 11_react/my-app/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import JsxUse from './chapter3/3.4/JsxUse';
 import Library from './chapter3/Library';
 import Clock from './chapter4/Clock';
+import PropUse from './chapter5/5.3/PropUse';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -20,11 +21,16 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // 4장 예제
 // 1초 마다 Clock 컴포넌트를 root div에 렌더링 하는 코드
-setInterval(() => {
-  root.render(
-    <Clock />
-  );
-}, 1000);
+// setInterval(() => {
+//   root.render(
+//     <Clock />
+//   );
+// }, 1000);
+
+// 5장 예제
+root.render(
+  <PropUse />
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
