7bb9d30bcabd33e8bc42223d10391d4a4b6bba71
goni.kim <geoblo@naver.com>
2023-01-19 13:58:01 +0900

    css 업

---
+++ 11_react/my-app/src/chapter5/Comment.jsx
@@ -0,0 +1,34 @@
+// CSS 스타일 작성(인라인 스타일로 넣어줄 객체 형태)
+const styles = {
+  wrapper: {
+    margin: 8,
+    padding: 8,
+    display: "flex",
+    flexDirection: "row",
+    border: "1px solid grey",
+    borderRadius: 16,
+  },
+  imageContainer: {},
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  contentContainer: {
+    marginLeft: 8,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  nameText: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  commentText: {
+    color: "black",
+    fontSize: 16,
+  },
+};
+// 사용할 이미지 경로
+// https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png
