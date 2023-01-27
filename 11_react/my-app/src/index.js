662648b0a40345adf492b8e57fd2a8298ecc2af7
goni.kim <geoblo@naver.com>
2023-01-27 18:58:00 +0900

    수업 자료 업

--- 11_react/my-app/src/index.js
+++ 11_react/my-app/src/index.js
@@ -30,6 +30,12 @@ import Mailbox from './chapter9/9.3/Mailbox';
 import LoginControlRefactoring from './chapter9/9.3/LoginControlRefactoring';
 import MainPage from './chapter9/9.4/MainPage';
 import LandingPage from './chapter9/LandingPage';
+import NumberList from './chapter10/10.1/NumberList';
+import AttendanceBook from './chapter10/AttendanceBook';
+import NameForm from './chapter11/11.2/NameForm';
+import EssayForm from './chapter11/11.3/EssayForm';
+import FlavorForm from './chapter11/11.3/FlavorForm';
+import FileInput from './chapter11/11.3/FileInput';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -87,13 +93,28 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // 9장 예제
 const messages = ['React', 'Re: React', 'Re:Re: React'];
 // const messages = [];
+// root.render(
+//   // <Greeting isLoggedIn={true} />
+//   // <LoginControl />
+//   // <Mailbox unreadMessages={messages} />
+//   // <LoginControlRefactoring />
+//   // <MainPage />
+//   <LandingPage />
+// );
+
+// 10장 예제
+const numbers = [1, 2, 3, 4, 5];
+// root.render(
+//   // <NumberList numbers={numbers} />
+//   <AttendanceBook />
+// );
+
+// 11장 예제
 root.render(
-  // <Greeting isLoggedIn={true} />
-  // <LoginControl />
-  // <Mailbox unreadMessages={messages} />
-  // <LoginControlRefactoring />
-  // <MainPage />
-  <LandingPage />
+  // <NameForm />
+  // <EssayForm />
+  // <FlavorForm />
+  <FileInput />
 );
 
 // If you want to start measuring performance in your app, pass a function
